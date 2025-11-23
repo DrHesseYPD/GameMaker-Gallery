a3222594c6416ba688f31c34fec0d51101a0b262
DrHesseYPD <hesseg6@hwbcymru.net>
2025-11-23 13:03:50 +0000

    Edit sites thru sites.js
    Code changed so site information can be changed by editing sites.js.
    No changes to the html code required

---
+++ sites.js
@@ -0,0 +1,32 @@
+// sites.js
+
+// Each entry can omit any field except "text"
+window.SITES = [
+	{
+        url: "https://drhesseypd.github.io/TestGameMakerSite/",
+        text: "Test GameMaker HTML Game",
+        color: "#4a6cf7",
+        icon: "fa-solid fa-football"
+    },
+	
+	{
+		url: "",   // missing or empty → shows warning
+		text: "Cozy Farm Game",
+		icon: "fa-solid fa-gamepad"
+	},  
+	
+	{
+        url: "https://drhesseypd.github.io/RPG-test/",
+        text: "Test GameMaker HTML - movement",
+        color: "#e67e22",
+        icon: "fa-solid fa-gamepad"
+    },
+  
+	  {
+		url: "https://drhesseypd.github.io/TestGameMakerSite/",
+        text: "Another test GameMaker HTML Game",
+		// color + icon omitted → default will be used
+	  }
+];
+
+
